6a3d2f313ecda6ec72fa7253d82e855a252ada9e
Chris Moscoso <Christophermoscoso@gmail.com>
2020-04-30 14:19:29 -0400

    Fixed lint for exercise component

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'exercise',
-  templateUrl: './exercise.component.html',
-  styleUrls: ['./exercise.component.scss'],
+  'selector': 'exercise',
+  'templateUrl': './exercise.component.html',
+  'styleUrls': ['./exercise.component.scss'],
 })
 export class ExerciseComponent implements OnInit {
 
