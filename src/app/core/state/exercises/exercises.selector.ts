8d8ccf1f84b23985e81b894c71c7d27bdd498acc
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-16 19:14:41 -0400

    Renaming get to select for selectors

--- src/app/core/state/exercises/exercises.selector.ts
+++ src/app/core/state/exercises/exercises.selector.ts
@@ -4,20 +4,20 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { exerciseAdapter } from './exercises.reducer';
 
 /**
- * Gets the top-level store property named 'exercises' of the state tree.
+ * Gets the top-level state property named 'exercises' of the store tree.
  */
 /* Note: createFeatureSelector allows us to get a top-level feature state
  * property of the state tree simply by calling it out by its feature name.
  */
-export const getState = createFeatureSelector < ExercisesState > ('exercises');
+export const selectState = createFeatureSelector < ExercisesState > ('exercises');
 export const {
     selectIds,
     selectEntities,
     selectAll,
     selectTotal,
-} = exerciseAdapter.getSelectors(getState);
+} = exerciseAdapter.getSelectors(selectState);
 
-export const getExerciseByID = (exerciseID: string) => createSelector(
-    getState,
+export const selectExerciseByID = (exerciseID: string) => createSelector(
+    selectState,
     (state: ExercisesState) => state.entities[exerciseID]
 );
