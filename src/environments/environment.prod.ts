cff814bedf39f15e399cb365305ab502957d47e1
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-22 19:39:32 -0400

    Moved firebase config object to environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-    'production': true
+    'production': true,
+    'firebase': {
+        'apiKey': 'AIzaSyDvyIyV5SeiGc5VR9kNulMsRE4qnnkEYeA',
+        'authDomain': 'strengthrx-protocols.firebaseapp.com',
+        'databaseURL': 'https://strengthrx-protocols.firebaseio.com',
+        'projectId': 'strengthrx-protocols',
+        'storageBucket': 'strengthrx-protocols.appspot.com',
+        'messagingSenderId': '71599561823',
+        'appId': '1:71599561823:web:4bec2e15b7ee4ad547cfea',
+        'measurementId': 'G-90WG2D041V'
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    'production': false
+    'production': false,
+    'firebase': {
+        'apiKey': 'AIzaSyDvyIyV5SeiGc5VR9kNulMsRE4qnnkEYeA',
+        'authDomain': 'strengthrx-protocols.firebaseapp.com',
+        'databaseURL': 'https://strengthrx-protocols.firebaseio.com',
+        'projectId': 'strengthrx-protocols',
+        'storageBucket': 'strengthrx-protocols.appspot.com',
+        'messagingSenderId': '71599561823',
+        'appId': '1:71599561823:web:4bec2e15b7ee4ad547cfea',
+        'measurementId': 'G-90WG2D041V'
+    }
 };
 
 /*
@@ -13,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
