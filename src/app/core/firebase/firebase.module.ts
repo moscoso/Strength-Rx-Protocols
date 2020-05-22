2af3f7c8253cb09b0d1e11ca7a54e69155aa0a83
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-22 19:39:15 -0400

    Enabled persistence for AngularFirestore

--- src/app/core/firebase/firebase.module.ts
+++ src/app/core/firebase/firebase.module.ts
@@ -4,23 +4,15 @@ import { AngularFireModule } from '@angular/fire';
 import { AuthService } from './auth.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { ExerciseService } from './entity/exercise.service';
-import { AngularFirestoreModule } from '@angular/fire/firestore/firestore.module';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 
 @NgModule({
     'declarations': [],
     'imports': [
         CommonModule,
-        AngularFireModule.initializeApp({
-            'apiKey': 'AIzaSyDvyIyV5SeiGc5VR9kNulMsRE4qnnkEYeA',
-            'authDomain': 'strengthrx-protocols.firebaseapp.com',
-            'databaseURL': 'https://strengthrx-protocols.firebaseio.com',
-            'projectId': 'strengthrx-protocols',
-            'storageBucket': 'strengthrx-protocols.appspot.com',
-            'messagingSenderId': '71599561823',
-            'appId': '1:71599561823:web:4bec2e15b7ee4ad547cfea',
-            'measurementId': 'G-90WG2D041V'
-        }),
+        AngularFireModule.initializeApp(environment.firebase),
         AngularFireAuthModule,
         AngularFirestoreModule.enablePersistence(),
     ],
