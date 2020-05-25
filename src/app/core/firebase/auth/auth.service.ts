76639e1f488f66b0aac714778a9041df6f7454a7
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-25 01:44:18 -0400

    Moved auth service. Refactored auth functions to work with v5 AngularFireAuth

---
+++ src/app/core/firebase/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import {
+    Injectable
+} from '@angular/core';
+import {
+    Observable
+} from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+
+/**
+ * This service is responsible for user authentication with Firebase.
+ */
+@Injectable({ 'providedIn': 'root' })
+export class AuthService {
+
+    constructor(
+        private firebaseAuth: AngularFireAuth,
+    ) {}
+
+    /**
+     * Observable of the currently signed-in user (or null).
+     */
+    getUser(): Observable < firebase.User > {
+        return this.firebaseAuth.user;
+    }
+
+    /**
+     * Initiates an account creation with Firebase using email and password credentials
+     * @param email the email address to register the account with
+     * @param password the password to regsiter the account with
+     */
+    async createUserWithEmailAndPassword(email: string, password: string): Promise < firebase.auth.UserCredential > {
+        return this.firebaseAuth.auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    /**
+     * Initiates a sign in to Firebase with email and password credentials
+     * @param email the email address of the user
+     * @param password the password of the user
+     */
+    async signInWithEmailAndPassword(email: string, password: string): Promise < firebase.auth.UserCredential > {
+        return this.firebaseAuth.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    /**
+     * Initiates a sign in to Firebase anonymously
+     */
+    async signInAsGuest(): Promise < firebase.auth.UserCredential > {
+        return this.firebaseAuth.auth.signInAnonymously();
+    }
+
+    /**
+     * Initiates a password reset with Firebase
+     * @param email the address associated with the account to initiate the password reset
+     */
+    async sendPasswordResetEmail(email: string): Promise < void > {
+        return this.firebaseAuth.auth.sendPasswordResetEmail(email);
+    }
+
+    /**
+     * Initiates a sign out with Firebase. The authenticated user will be set to null.
+     */
+    async signOut(): Promise < void > {
+        return this.firebaseAuth.auth.signOut();
+    }
+
+}
