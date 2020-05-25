3d8ac11e43259ce3a9fe78c9803fe0c3d5f690b2
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-25 13:59:15 -0400

    Added Tab Synchornization for Offline mode

--- src/app/core/firebase/firebase.module.ts
+++ src/app/core/firebase/firebase.module.ts
@@ -12,7 +12,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
         CommonModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireAuthModule,
-        AngularFirestoreModule.enablePersistence(),
+        AngularFirestoreModule.enablePersistence({'synchronizeTabs': true}),
     ],
     'providers': []
 })
