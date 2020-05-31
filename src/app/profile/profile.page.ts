f62ab460cf6321b989017e192fed182d966d1c6d
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-30 22:46:39 -0400

    Added functionality to the profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Profile } from '../core/state/profile/profile.state';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AuthStoreDispatcher } from '../core/state/auth/auth.dispatcher';
+import * as fromProfile from '../core/state/profile/profile.selector';
+import { AllRequested } from '../core/state/profile/profile.actions';
 
 @Component({
     'selector': 'app-profile',
@@ -7,8 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-    constructor() {}
 
-    ngOnInit() {}
+    public profile$: Observable < Profile > ;
+    public isEdit = false;
+
+    constructor(
+        public store: Store,
+        public authStore: AuthStoreDispatcher
+    ) {}
+
+    ngOnInit() {
+        this.store.dispatch(new AllRequested());
+        this.fetchProfile();
+    }
+
+    async fetchProfile() {
+        this.profile$ = this.store.select(fromProfile.selectUserProfile);
+    }
+
+    getAvatar(profile: Profile) {
+        return profile.photoURL != null && profile.photoURL.length > 0 ? profile.photoURL : this.getInitialsAvatar(profile);
+    }
+
+    getInitialsAvatar(profile: Profile) {
+        return `https://ui-avatars.com/api/?name=${profile.firstName}+${profile.lastName}`;
+    }
+
+    logout() {
+        this.authStore.logout();
+    }
 
 }
