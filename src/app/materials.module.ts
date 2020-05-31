40470c68a0d2d0b6ab882da27907da9f191ba67a
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-31 07:52:29 -0400

    Added MatDatepickerModule

--- src/app/materials.module.ts
+++ src/app/materials.module.ts
@@ -5,6 +5,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatOptionModule } from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 const modules = [
     MatInputModule,
@@ -13,6 +15,7 @@ const modules = [
     MatOptionModule,
     MatSelectModule,
     MatRadioModule,
+    MatDatepickerModule
 ];
 
 @NgModule({
