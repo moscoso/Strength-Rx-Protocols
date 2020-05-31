50fc1153d0a15250ecfd05f81769876d6855b4c9
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-31 09:10:35 -0400

    Created CreateProfile page

--- src/app/create-profile/create-profile.module.ts
+++ src/app/create-profile/create-profile.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CreateProfilePageRoutingModule } from './create-profile-routing.module';
 
 import { CreateProfilePage } from './create-profile.page';
+import { MaterialsModule } from '../materials.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CreateProfilePageRoutingModule
-  ],
-  declarations: [CreateProfilePage]
+    'imports': [
+        CommonModule,
+        ReactiveFormsModule,
+        MaterialsModule,
+        FormsModule,
+        IonicModule,
+        CreateProfilePageRoutingModule
+    ],
+    'declarations': [CreateProfilePage]
 })
 export class CreateProfilePageModule {}
--- src/app/create-profile/create-profile.page.ts
+++ src/app/create-profile/create-profile.page.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../core/state/app.state';
+import { Profile, ClientApplicationStatus } from '../core/state/profile/profile.state';
+import { CreateRequested } from '../core/state/profile/profile.actions';
+import { take } from 'rxjs/operators';
+import * as fromAuth from '../core/state/auth/auth.selector';
 
 @Component({
-  selector: 'app-create-profile',
-  templateUrl: './create-profile.page.html',
-  styleUrls: ['./create-profile.page.scss'],
+    'selector': 'app-create-profile',
+    'templateUrl': './create-profile.page.html',
+    'styleUrls': ['./create-profile.page.scss'],
 })
 export class CreateProfilePage implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
+    requestInProgress$: Observable < boolean > ;
 
-  ngOnInit() {
-  }
+    firstName = new FormControl('', Validators.required);
+    lastName = new FormControl('', Validators.required);
+    sex = new FormControl('', Validators.required);
+    birthday = new FormControl('', Validators.required);
+    feet = new FormControl('', [Validators.required, Validators.min(3), Validators.max(7)]);
+    inches = new FormControl('', [Validators.required, Validators.min(0), Validators.max(11)]);
+    constructor(private store: Store) {
+
+    }
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            'firstName': this.firstName,
+            'lastName': this.lastName,
+            'sex': this.sex,
+            'birthday': this.birthday,
+            'feet': this.feet,
+            'inches': this.inches,
+        });
+        this.requestInProgress$ = this.store.select(
+            (state: AppState) => state.profiles.requestInProgress
+        );
+    }
+
+    async onSubmit(form) {
+        const userID = await this.store.select(
+            fromAuth.selectUserID
+        ).pipe(take(1)).toPromise();
+
+        const profile: Profile = {
+            'id':  userID,
+            'firstName': form.firstName,
+            'lastName': form.lastName,
+            'isClient': false,
+            'isTrainer': false,
+            'joined': new Date(),
+            'photoURL': '',
+            'sex': form.sex,
+            'birthday': form.birthday,
+            'height': {
+                'feet': form.feet,
+                'inches': form.inches
+            },
+            'clientApplicationStatus': ClientApplicationStatus.NOT_STARTED
+        };
+        this.store.dispatch(new CreateRequested(profile));
+    }
 
 }
