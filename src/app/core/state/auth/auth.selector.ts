549e59b21d4548b217e30ea583a992139fdaa988
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-31 07:08:16 -0400

    Created an auth selector for userID

--- src/app/core/state/auth/auth.selector.ts
+++ src/app/core/state/auth/auth.selector.ts
@@ -1,4 +1,9 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AuthState } from './auth.state';
 
 export const selectState = createFeatureSelector < AuthState > ('auth');
+
+export const selectUserID = createSelector(
+    selectState,
+    (state: AuthState) => state.userID
+);
