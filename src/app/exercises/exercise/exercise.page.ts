07accef2f7e667e11483299c9b7ef49000059334
Chris Moscoso <Christophermoscoso@gmail.com>
2020-06-01 16:40:27 -0400

    Began work on Edit and Delete actions for an Exercise

--- src/app/exercises/exercise/exercise.page.ts
+++ src/app/exercises/exercise/exercise.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as fromExercises from '../../core/state/exercises/exercises.selector';
-import { AllRequested } from 'src/app/core/state/exercises/exercises.actions';
+import { AllRequested, DeleteRequested } from 'src/app/core/state/exercises/exercises.actions';
 import { filter, take } from 'rxjs/operators';
 import { AppState } from 'src/app/core/state/app.state';
+import { Exercise } from 'src/app/core/state/exercises/exercises.state';
+import { selectUserIsTrainer } from 'src/app/core/state/profile/profile.selector';
+import { ModalController, ActionSheetController } from '@ionic/angular';
+
 
 @Component({
     'selector': 'app-exercise',
@@ -13,16 +17,22 @@ import { AppState } from 'src/app/core/state/app.state';
 })
 export class ExercisePage implements OnInit {
 
-    exercise$: Observable < any > ;
+    exercise$: Observable < Exercise > ;
+    isTrainer$: Observable < boolean > ;
 
     constructor(
-        public store: Store
+        public store: Store,
+        public modalCtrl: ModalController,
+        public actionSheetCtrl: ActionSheetController,
     ) {}
 
     ngOnInit() {
         this.store.dispatch(new AllRequested());
         this.exercise$ = this.store.select(
             fromExercises.selectExerciseByRouteURL
+        );
+        this.isTrainer$ = this.store.select(
+            selectUserIsTrainer
         );
     }
 
@@ -34,6 +44,33 @@ export class ExercisePage implements OnInit {
         ).toPromise().then(() => {
             event.target.complete();
         });
+    }
+
+    // requestEdit(): void {
+
+    // }
+
+    async showActionSheetToDelete(): Promise < void > {
+        const actionSheet = await this.actionSheetCtrl.create({
+            'id': 'exerciseDelete',
+            'header': 'Are you sure you want to delete?',
+            'buttons': [
+            {
+                'text': 'Delete',
+                'role': 'destructive',
+                'icon': 'trash',
+                'handler': () => {this.requestDelete(); }
+            }, {
+                'text': 'Cancel',
+                'role': 'cancel'
+            }],
+        });
+        actionSheet.present();
+    }
+
+    async requestDelete(): Promise < void > {
+        const exercise = await this.exercise$.pipe(take(1)).toPromise();
+        this.store.dispatch(new DeleteRequested(exercise.id));
     }
 
 }
