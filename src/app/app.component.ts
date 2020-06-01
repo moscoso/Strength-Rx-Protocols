9a87f5fb3392e9a2ca2e5768e6002c8b45a36a43
Chris Moscoso <Christophermoscoso@gmail.com>
2020-06-01 16:38:13 -0400

    Added Check In to nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,11 @@ export class AppComponent implements OnInit {
             'icon': '',
             'link': '/profile'
         },
+        {
+            'label': 'Check In',
+            'icon': '',
+            'link': '/check-in',
+        },
     ];
 
     public accountPages: MenuItem [] = [
