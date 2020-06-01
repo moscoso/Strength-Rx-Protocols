c7901a206718d2e71ffc5eda468736d72b0891bc
Chris Moscoso <Christophermoscoso@gmail.com>
2020-05-31 22:39:45 -0400

    Created Workouts Preview component

---
+++ src/app/workouts/workout-preview/workout-preview.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    'selector': 'app-workout-preview',
+    'templateUrl': './workout-preview.component.html',
+    'styleUrls': ['./workout-preview.component.scss'],
+})
+export class WorkoutPreviewComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {}
+
+}
