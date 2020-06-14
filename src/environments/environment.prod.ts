410069526094e5889ababda5d17debe48c9f5aa2
Chris Moscoso <Christophermoscoso@gmail.com>
2020-06-14 04:20:03 -0400

    Generated new key and moved it out of view of repository

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
+import {firebase} from '../config/firebase';
+
 export const environment = {
     'production': true,
-    'firebase': {
-        'apiKey': 'AIzaSyDvyIyV5SeiGc5VR9kNulMsRE4qnnkEYeA',
-        'authDomain': 'strengthrx-protocols.firebaseapp.com',
-        'databaseURL': 'https://strengthrx-protocols.firebaseio.com',
-        'projectId': 'strengthrx-protocols',
-        'storageBucket': 'strengthrx-protocols.appspot.com',
-        'messagingSenderId': '71599561823',
-        'appId': '1:71599561823:web:4bec2e15b7ee4ad547cfea',
-        'measurementId': 'G-90WG2D041V'
-    }
+    'firebase': firebase
 };
