10c9dce3b8f466411a4935d4e77a59709d6acd1c
Chris Moscoso <Christophermoscoso@gmail.com>
2020-06-29 23:18:19 -0400

    Refactored function in ExerciseService for bugfix and better readability

--- src/app/core/firebase/exercise/exercise.service.ts
+++ src/app/core/firebase/exercise/exercise.service.ts
@@ -3,10 +3,9 @@ import { EntityService } from '../entity/entity.service';
 import { Exercise, INIT_EXERCISE } from '../../state/exercises/exercises.state';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FirebaseModule } from '../firebase.module';
+import { transformToSlug } from 'src/util/slug/transformToSlug';
 
-@Injectable({
-    'providedIn': FirebaseModule,
-})
+@Injectable({'providedIn': FirebaseModule, })
 export class ExerciseService extends EntityService < Exercise > {
     constructor(
         public firestore: AngularFirestore,
@@ -17,15 +16,16 @@ export class ExerciseService extends EntityService < Exercise > {
 
     /**
      * Create a new Firestore document for the entity.
-     * The ID of the entity will be the exercise name.
+     * The ID of the entity will be a slug of the exercise name.
      * @param exercise the exercise entity being created
      */
     async create(exercise: Exercise): Promise < Exercise > {
-        const doc = await this.firestore.doc(`exercises/${exercise.name}`).ref.get();
+        const docID = transformToSlug(exercise.name);
+        const doc = await this.firestore.doc(`exercises/${docID}`).ref.get();
         if (doc.exists) {
-            throw new Error(`Exercises ${exercise.name} already exists`);
+            throw new Error(`Exercise of ID ${docID} already exists`);
         }
-        await this.entityCollection.doc(`${exercise.name}`).set(exercise);
+        await this.entityCollection.doc(`${docID}`).set(exercise);
         return exercise;
     }
 }
