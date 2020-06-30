3c54656fb58ce09f41230568b0fff014e970840a
Chris Moscoso <Christophermoscoso@gmail.com>
2020-06-29 23:21:50 -0400

    Throwing error on transformToSlug utility function when undefined is passed in as a parameter

--- src/util/slug/transformToSlug.ts
+++ src/util/slug/transformToSlug.ts
@@ -1,9 +1,12 @@
 /**
  * Transforms a string into a slug, which is meant to be a user friendly URI.
- * 
+ *
  * @param string the string to transform into a slug
  * @returns a string that is lowercase and that replaces all white space with dashes '-'
  */
-export function transformToSlug(string: string): string {
-    return string.trim().replace(/\s+/g, '-').toLowerCase();
+export function transformToSlug(s: string): string {
+    if (s == null) {
+        throw new Error(`Cannot transform undefined into a slug`);
+    }
+    return s.trim().replace(/\s+/g, '-').toLowerCase();
 }
