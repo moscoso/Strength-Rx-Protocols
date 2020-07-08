c34c8d0d83567db797850ffbf7244e91daa2f9e5
Chris Moscoso <Christophermoscoso@gmail.com>
2020-07-08 17:51:38 -0400

    Added About Section in LandingPage and made the Header sticky

--- src/app/home/landing/landing.page.ts
+++ src/app/home/landing/landing.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, ViewChild, ElementRef, AfterViewInit, AfterViewChecked, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { RegisterPage } from 'src/app/account/register/register.page';
+import { environment } from 'src/environments/environment';
 
 @Component({
     'selector': 'app-landing',
@@ -8,30 +10,58 @@ import { MenuController } from '@ionic/angular';
 })
 export class LandingPage implements OnInit, AfterViewInit {
 
+    @ViewChild('header', { 'read': ElementRef }) header: ElementRef < HTMLIonHeaderElement > ;
     @ViewChild('video') video: ElementRef < HTMLVideoElement > ;
 
+
+    headerHeight = 0;
+
     constructor(
-        public menuController: MenuController
+        public modalController: ModalController
     ) {}
 
     ngOnInit() {
-        this.getMenu();
-    }
-
-    async getMenu() {
-        console.log(await this.menuController.getMenus());
-        const menu = (await this.menuController.getMenus())[0];
-        console.log(menu);
+        window.addEventListener('resize', (e) => {
+            this.setHeaderHeight();
+        });
     }
 
     ngAfterViewInit() {
+        this.setHeaderHeight();
+        this.startVideo();
+    }
+
+    startVideo() {
         if (this.video) {
             this.video.nativeElement.muted = true;
             this.video.nativeElement.play();
             this.video.nativeElement.volume = .5;
         }
-
-
     }
 
+    setHeaderHeight() {
+        const height = this.header.nativeElement.clientHeight;
+        if (this.header && height !== 0) {
+            this.headerHeight = height;
+        }
+    }
+
+    /**
+     * Get a resource from the Firebase's project storage bucket
+     * @param slug the slug of the resource
+     */
+    getStorageURL(slug: string) {
+        const storageBucket = environment.firebase.storageBucket;
+        return `https://firebasestorage.googleapis.com/v0/b/${storageBucket}/o/${slug}`;
+    }
+
+
+    async openModal() {
+        const modal = await this.modalController.create({
+            'id': 'register',
+            'component': RegisterPage
+        });
+        await modal.present();
+        return;
+    }
 }
