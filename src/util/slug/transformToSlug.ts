9ed9ba33bd48e01749b0f5aee507b1ad42c77447
Chris Moscoso <Christophermoscoso@gmail.com>
2020-08-14 11:45:57 -0400

    Adjusted transform to slug logic to include dashes in slug

--- src/util/slug/transformToSlug.ts
+++ src/util/slug/transformToSlug.ts
@@ -2,11 +2,12 @@
  * Transforms a string into a slug, which is meant to be a user friendly URI.
  *
  * @param string the string to transform into a slug
- * @returns a string that is lowercase and that replaces all white space with dashes '-'
+ * @returns a string that is lowercase, only includes dashes and alphanumerical characters,
+ *  and that replaces all white space with dashes '-'
  */
 export function transformToSlug(s: string): string {
     if (s == null) {
         throw new Error(`Cannot transform undefined into a slug`);
     }
-    return s.trim().replace(/[^0-9a-z\s]/gi, '').replace(/\s+/g, '-').toLowerCase();
+    return s.trim().replace(/[^0-9a-z\-\s]/gi, '').replace(/\s+/g, '-').toLowerCase();
 }
