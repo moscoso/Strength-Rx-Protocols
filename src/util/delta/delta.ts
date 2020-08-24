f18d1577f76ae8f913d3b14d2bbd92b523394377
Chris Moscoso <Christophermoscoso@gmail.com>
2020-08-24 18:24:25 -0400

    Bug Fix for Delta utility class when comparing objects with arrays

--- src/util/delta/delta.ts
+++ src/util/delta/delta.ts
@@ -1,9 +1,13 @@
+/**
+ * A utility class to help return the difference between two objects by comparing the object using deep equality.
+ */
 export class Delta {
 
     /**
      * Return an object that represents the difference between two objects.
-     * Order matters! The returned difference object will use the values from a
-     * Only works with shallow objects. Nested objects will not be tested for equality.
+     * Order matters! The returned difference object will use the values from b
+     *
+     * `Only works with shallow objects. Nested objects will not be tested for equality.`
      */
     public static object(a: object, b: object): object {
         const diff = Object.assign({}, a, b);
@@ -11,7 +15,8 @@ export class Delta {
             const valueA = a[key];
             const valueB = b[key];
             let valuesMatch: boolean;
-            if (valueA instanceof Array && valueB instanceof Array) {
+            const bothAreArrays = valueA instanceof Array && valueB instanceof Array;
+            if (bothAreArrays) {
                 valuesMatch = this.arraysMatch(valueA, valueB);
             } else {
                 valuesMatch = valueA === valueB;
@@ -26,15 +31,16 @@ export class Delta {
 
     private static arraysMatch(a: any[], b: any[]): boolean {
         const differentSizes = a.length !== b.length;
-        if (differentSizes) { return false;  }
+        if (differentSizes) { return false; }
 
+        let arraysMatch = true;
         a.forEach((value, index) => {
             const mismatchFound = a[index] !== b[index];
             if (mismatchFound) {
-                return false;
+                arraysMatch = false;
             }
         });
 
-        return true;
+        return arraysMatch;
     }
 }
