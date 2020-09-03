fc82da71ad7fe6937ef118aa3e3985f1769b66ba
Chris Moscoso <Christophermoscoso@gmail.com>
2020-09-03 09:15:48 -0400

    Bug fix for Stripe webhooks

--- functions/src/stripe/webhooks.ts
+++ functions/src/stripe/webhooks.ts
@@ -10,29 +10,29 @@ import { createClient } from '../strengthrx/clients';
 
 export async function webhookHandler(data: any): Promise<FirebaseFirestore.WriteResult> {
     const customerID = data.customer;
-    const subscriptionID = data.subscription;
+    const subscriptionID = data.id;
     const customer = await stripe.customers.retrieve(customerID);
 
     if (customer.deleted) {
         throw new Error(`Cannot execute webhook because customer ${customerID} is deleted`)
     } else {
-        const userID = customer.metadata.firebaseUID;
+        const userID = customer.metadata.firebaseUserID;
         const subscription = await stripe.subscriptions.retrieve(subscriptionID);
         return await updateSubscription(userID, subscription)
     }
 };
 
 export async function subscriptionCreatedHandler(data: any): Promise<FirebaseFirestore.WriteResult> {
-    console.log(data);
     const customerID = data.customer;
-    const subscriptionID = data.subscription;
+    const subscriptionID = data.id;
+
     const customer = await stripe.customers.retrieve(customerID);
 
     if (customer.deleted) {
         throw new Error(`Cannot execute webhook because customer ${customerID} is deleted`)
     } else {
-        const userID = customer.metadata.firebaseUID;
-        const subscription = await stripe.subscriptions.retrieve(subscriptionID);
+        const userID = customer.metadata.firebaseUserID;
+        const subscription = await stripe.subscriptions.retrieve(subscriptionID);        
         await createClient(userID, subscription);
         if(subscription.plan === null) {throw new Error(`Subscription plan was null`)}
         return await activateSubscription(userID, subscription.plan.id, subscription.id)
@@ -75,15 +75,14 @@ export const subscriptionCreatedWebhook = functions.https.onRequest(
                 stripeCreateSubscriptionSignature
             );
             const data = event.data.object;
-            subscriptionCreatedHandler(data).then(() => {
-                res.sendStatus(200);
+            subscriptionCreatedHandler(data).then((result) => {
+                res.status(200).send(result);
             }).catch((reason)=> {
+                console.log(reason);
                 res.status(400).send(reason);
             });
-            
         } catch (err) {
-            console.error(err);
-            
+            console.log(err);
         }
     }
 );
