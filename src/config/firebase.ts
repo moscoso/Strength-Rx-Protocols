9ee388fa10e5a515f47294c2b25b24943c8f6c86
Chris Moscoso <Christophermoscoso@gmail.com>
2022-01-14 12:52:17 -0500

    fix CI/CD jobs by no longer ignoring publishable keys

---
+++ src/config/firebase.ts
@@ -0,0 +1,23 @@
+export const firebase = {
+    'production': {
+        'apiKey': 'AIzaSyAoSkznyuai4DsnuIdiH6gDn2RIQwspAP8',
+        'authDomain': 'strengthrx-protocols.firebaseapp.com',
+        'databaseURL': 'https://strengthrx-protocols.firebaseio.com',
+        'projectId': 'strengthrx-protocols',
+        'storageBucket': 'strengthrx-protocols.appspot.com',
+        'messagingSenderId': '71599561823',
+        'appId': '1:71599561823:web:f15e79d2a98c540647cfea',
+        'measurementId': 'G-GY2ZRQ4YL8'
+    },
+
+    'staging': {
+        'apiKey': 'AIzaSyDuw9t-kba3RYeYoQOj_kpbnnHOxbpsJ1Q',
+        'authDomain': 'strengthrx-pro-test.firebaseapp.com',
+        'databaseURL': 'https://strengthrx-pro-test.firebaseio.com',
+        'projectId': 'strengthrx-pro-test',
+        'storageBucket': 'strengthrx-pro-test.appspot.com',
+        'messagingSenderId': '865632525995',
+        'appId': '1:865632525995:web:5f37d2486b58c18b0421ee',
+        'measurementId': 'G-08BRD8YTDK'
+    }
+};
---
+++ src/config/stripe.ts
@@ -0,0 +1,9 @@
+/**
+ * The publishable keys that enable the Stripe API
+ */
+export const stripe = {
+    'production': 'pk_live_T6qB2lI9q51daLjTsDEX5tPX00Hzpq5PY3',
+    'staging': 'pk_test_51GuLd0LM5gy7nmLDEXT328ttXY6al7FlGMHFdMjeouQA5ueFc2AENeiESgOjpoEoulTB7gnPHRLElLxS2Jz4IXea00VOHd9qXF'
+};
+
+
